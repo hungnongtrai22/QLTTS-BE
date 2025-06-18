fe08edd7e33cde1f038c110318826be54c69a987
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-06-18 11:25:52 +0700

    Update getInternByMonth.ts

--- src/pages/api/study/getInternByMonth.ts
+++ src/pages/api/study/getInternByMonth.ts
@@ -31,7 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     if (!study) {
-      return res.status(404).json({ message: 'Study not found' });
+      return res.status(200).json(null);
     }
 
     return res.status(200).json({ study });
