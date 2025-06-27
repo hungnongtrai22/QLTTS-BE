094abfa910283af42b2554c38ec082e291faf558
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-06-27 09:13:49 +0700

    Update index.ts

--- src/pages/api/user/[id]/index.ts
+++ src/pages/api/user/[id]/index.ts
@@ -4,7 +4,8 @@ import cors from 'src/utils/cors';
 // _mock
 import Intern from 'src/models/intern';
 import db from 'src/utils/db';
-
+import TradeUnion from 'src/models/tradeUnion';
+import Company from 'src/models/company';
 // ----------------------------------------------------------------------
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -12,14 +13,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await cors(req, res);
     await db.connectDB();
 
-
-    const intern = await Intern.findById(req.query.id); 
-    res.status(200).json({
+    const intern = await Intern.findById(req.query.id)
+      .populate({ path: 'tradeUnion', model: TradeUnion })
+      .populate({ path: 'companySelect', model: Company });
+    return res.status(200).json({
       intern,
     });
   } catch (error) {
     console.error('[Product API]: ', error);
-    res.status(400).json({
+    return res.status(400).json({
       message: error,
     });
   }
