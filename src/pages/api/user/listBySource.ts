e102ac579b7e914776ab1e80bc5d9551761c7516
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-07-29 08:26:02 +0700

    Create listBySource.ts

---
+++ src/pages/api/user/listBySource.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+// utils
+import cors from 'src/utils/cors';
+// _mock
+import Intern from 'src/models/intern';
+import db from 'src/utils/db';
+import TradeUnion from 'src/models/tradeUnion';
+import Company from 'src/models/company';
+import Source from 'src/models/source';
+
+// ----------------------------------------------------------------------
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await cors(req, res);
+    await db.connectDB();
+
+    const interns = await Intern.find({
+      source: req.body.source,
+    })
+      .populate({ path: 'tradeUnion', model: TradeUnion })
+      .populate({ path: 'companySelect', model: Company })
+      .populate({ path: 'source', model: Source });
+
+    res.status(200).json({
+      interns,
+    });
+  } catch (error) {
+    console.error('[Intern API]: ', error);
+    res.status(400).json({
+      message: error,
+    });
+  }
+}
