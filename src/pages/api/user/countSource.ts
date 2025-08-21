44232137eef59a4ad97d76ec9810cd60a4e81cce
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-08-21 16:34:13 +0700

    Update countSource.ts

--- src/pages/api/user/countSource.ts
+++ src/pages/api/user/countSource.ts
@@ -23,9 +23,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         $match: {
           $or: [
             { status: 'study' },
-            { status: { $exists: false } },
-            { status: null },
-            { status: '' },
           ],
         },
       },
