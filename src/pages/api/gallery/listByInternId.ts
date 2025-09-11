cda10adfed9aa25290c234deeb206953c0c51176
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-09-11 16:41:36 +0700

    Update listByInternId.ts

--- src/pages/api/gallery/listByInternId.ts
+++ src/pages/api/gallery/listByInternId.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       filter = { status };
     }
 
-    const galleries = await Gallery.find(filter);
+    const galleries = await Gallery.find(filter).sort({ postedAt: -1 });
 
     return res.status(200).json({
       galleries,
