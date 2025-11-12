2e63dbb1547880d13cbddf4c976e790f4982dbe9
VU QUOC HUNG <vuquochung220402@gmail.com>
2025-11-12 10:12:42 +0700

    Create updatePublic.ts

---
+++ src/pages/api/study/updatePublic.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+// utils
+import cors from 'src/utils/cors';
+import Study from 'src/models/study';
+
+// _mock
+import db from '../../../utils/db';
+// ----------------------------------------------------------------------
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await cors(req, res);
+
+    if (req.method !== 'PUT') {
+      return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    await db.connectDB();
+
+    const { _id, isPublic } = req.body;
+
+    if (!_id) {
+      return res.status(400).json({ message: 'Missing Study ID (_id)' });
+    }
+
+    const updatedStudy = await Study.findByIdAndUpdate(
+      _id,
+      {
+        isPublic,
+      },
+      { new: true }
+    );
+
+    if (!updatedStudy) {
+      return res.status(404).json({ message: 'Study not found' });
+    }
+
+    return res.status(200).json({
+      study: updatedStudy,
+    });
+  } catch (error) {
+    console.error('[Update Study API]: ', error);
+    return res.status(400).json({
+      message: error,
+    });
+  }
+}
